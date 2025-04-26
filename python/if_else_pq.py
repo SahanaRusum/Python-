f0ca17d8440f0b130dd16328f9413daa81f37dc7
SahanaRusum <167613072+SahanaRusum@users.noreply.github.com>
2025-04-26 21:33:46 +0530

    if_else_pq.py

--- python/if_else_pq.py
+++ python/if_else_pq.py
@@ -1,4 +1,4 @@
-"""question 1:  Writeaprogramthataskstheusertoenteralengthincentimeters. If the user enters a negative
+"""question 1:  Write a program that asks the user to enter a length in centimeters. If the user enters a negative
  length, the program should tell the user that the entry is invalid. Otherwise, the program
  should convert the length to inches and print out the result. There are 2.54 centimeters in an
  inch """
